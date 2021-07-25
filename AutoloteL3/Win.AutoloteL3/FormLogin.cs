dbf3a43b3c7271a359ed1130edf0e984c51d5e97
BRYAN FRANCISCO ZAVALA MURILLO <bf.murillo@unah.hn>
2021-07-24 23:51:09 -0600

    clonando

---
+++ AutoloteL3/Win.AutoloteL3/FormLogin.cs
@@ -0,0 +1,59 @@
+﻿using BL.Ventas;
+using System;
+using System.Windows.Forms;
+
+namespace Win.AutoloteL3
+{
+    public partial class FormLogin : Form
+    {
+        SeguridadBL _seguridad;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            _seguridad = new SeguridadBL();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+
+
+            button1.Enabled = false;
+            button1.Text = "Verificando....";
+            Application.DoEvents();
+
+            var resultado = _seguridad.Autorizar(usuario, contrasena);
+
+            if (resultado == true )
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ususario o contrasena incorrecta");
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AutoloteL3/Win.AutoloteL3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.AutoloteL3
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMenu());
+        }
+        public static byte[] imagenToByteArray(Image ImagenIn)
+        {
+            var ms = new MemoryStream();
+            ImagenIn.Save(ms, ImagenIn.RawFormat);
+
+            return ms.ToArray();
+        }
+       
+    }
+}
