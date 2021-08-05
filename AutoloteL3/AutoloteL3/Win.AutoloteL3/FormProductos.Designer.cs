194f8f94e7c39f17e074300d5be4f9881ebc18e3
BRYAN FRANCISCO ZAVALA MURILLO <bf.murillo@unah.hn>
2021-08-05 16:13:16 -0600

    Asignación 9 ***15%

--- AutoloteL3/AutoloteL3/Win.AutoloteL3/FormProductos.Designer.cs
+++ AutoloteL3/AutoloteL3/Win.AutoloteL3/FormProductos.Designer.cs
@@ -392,7 +392,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(627, 275);
             this.Controls.Add(activoLabel);
             this.Controls.Add(this.activoCheckBox);
